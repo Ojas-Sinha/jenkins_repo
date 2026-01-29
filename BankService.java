4f9bbcfb0a7d334a1c9351490e5df820021a0f87
Ojas Sinha <ojassinha14@gmail.com>
2026-01-29 22:44:23 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -38,7 +38,7 @@ public class BankService {
         System.out.println("Initial Balance: " + acc.getBalance());
 
         // Perform operations
-        acc.deposit(600);
+        acc.deposit(800);
         System.out.println("Balance after deposit of 500: " + acc.getBalance());
 
         acc.withdraw(300);
